6db52adeb54f0165ea770511bf7da1ef977fff52
Igor Bonato <igorbonato99@hotmail.com>
2026-01-11 20:42:58 -0300

    Melhorando headers webmotors e ssl autocarro

--- webmotors.py
+++ webmotors.py
@@ -48,13 +48,19 @@ def main():
     print("--- Iniciando Webmotors ---")
 
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-        'Referer': 'https://www.webmotors.com.br/',
-        'Origin': 'https://www.webmotors.com.br',
-        'x-requested-with': 'XMLHttpRequest',
-        'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120"',
+        'authority': 'www.webmotors.com.br',
+        'accept': 'application/json, text/plain, */*',
+        'accept-language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+        'origin': 'https://www.webmotors.com.br',
+        'referer': 'https://www.webmotors.com.br/carros/rs?autocomplete=etios&perfil=carros&modelo=etios',
+        'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
         'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Linux"'
+        'sec-ch-ua-platform': '"Linux"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        'x-requested-with': 'XMLHttpRequest'
     }
 
     try:
